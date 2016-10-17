eafda84daaa708d3f95a2ec582830b2b6b8f9db7
muratrahmikurtul <muratrahmikurtul@gmail.com>
2016-10-17 09:44:38 +0300

    GOR-5 #time 5m - Fix seed.data for postgreqsl and fix record.rb

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :domain
 
-  validates :name, :type, :content, :ttl, presence: true
+  validates :name, :rtype, :content, :ttl, presence: true
 
   enum rtype: { SOA: 0, A: 1, AAAA: 2, NS: 3, PTR: 4, CNAME: 5, MX: 6, SRV: 7, TXT: 8, NAPTR: 9}
 
