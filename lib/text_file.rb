d67c9327eca88a9c76a89732fc0f66a2e3bfc909
hamdibayhan <hamdibayhan@gmail.com>
2016-10-18 17:25:33 +0300

    GOR-7 #time 5m - some improvements were done

--- lib/text_file.rb
+++ lib/text_file.rb
@@ -5,26 +5,16 @@ class FileProcess
   end
 
   def create_file_for_domain(text_file_name, domain_id)
-
-    tmp_file = File.new("./" + @directory_name + "/" + text_file_name + ".txt", "w+")
-    @records = Record.where(domain_id: domain_id)
-    if @records.length > 0
-      @records.each do |record|
-        if record.prio.present?
-          tmp_file.puts(record.name + "\t" + record.rtype + "\t" + record.content.to_s + "\t" + record.ttl.to_s)
-        else
-          tmp_file.puts(record.name + "\t" + record.rtype + "\t" + record.content.to_s + "\t" + record.ttl.to_s + "\t" + record.prio.to_s)
-        end
-      end
-    end
-    tmp_file.close
+    write_records_create_file(domain_id, text_file_name)
   end
 
   def create_file_for_record(domain_id)
-
     tmp_text_file_name = Domain.find(domain_id).name
-    tmp_file = File.new("./" + @directory_name + "/" + tmp_text_file_name + ".txt", "w+")
+    write_records_create_file(domain_id, tmp_text_file_name)
+  end
 
+  def write_records_create_file (domain_id, text_file_name)
+    tmp_file = File.new("./" + @directory_name + "/" + text_file_name + ".txt", "w+")
     @records = Record.where(domain_id: domain_id)
     @records.each do |record|
       if record.prio.present?
