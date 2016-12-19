3a3e49e0d8c705dcb14804075fe533dee53359f1
esrefv <esref.viduslu@gmail.com>
2016-12-19 17:48:28 +0300

    GOR-15 #time 5m - delete_domain_job bugs fixed

--- app/jobs/delete_domain_job.rb
+++ app/jobs/delete_domain_job.rb
@@ -1,9 +1,7 @@
 class DeleteDomainJob < ApplicationJob
   queue_as :default
-  puts "DeleteDomainJob"
 
   def perform(textfilename)
-    puts "Delete Perform içine girdi :D "
     GdnsdIo::FileOperations.new({filepath: APP_CONFIG['development_path']}).delete_text(textfilename)
   end
 end
